402d1f1bc1983911113ff8d8eaa84509656fcaa6
ViacheslavZN <zyrinviacheslav@gmail.com>
2023-01-27 11:14:38 +0300

    frontend+backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import AuthorList from './components/Author.js'
-
+import axios from 'axios'
 
 
 class App extends React.Component {
@@ -13,24 +13,16 @@ class App extends React.Component {
 }
 
 componentDidMount() {
-    const authors = [
-        {
-            'first_name': 'Фёдор',
-             'last_name': 'Достоевский',
-              'birthday_year': 1821
-        },
-        {
-                'first_name': 'Александр',
-                'last_name': 'Грин',
-                'birthday_year': 1880
-        },
-    ]
-this.setState(
-        {
-            'authors': authors
-        }
-       )
-    }
+   axios.get('http://127.0.0.1:8000/api/authors')
+        .then(response => {
+            const authors = response.data
+                this.setState(
+            {
+                'authors': authors
+            }
+        )
+    }).catch(error => console.log(error))
+}
 
     render () {
         return (
