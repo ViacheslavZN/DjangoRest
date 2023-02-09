fe9ac4b2f939358e9b407dfcd61da266023ef6de
ViacheslavZN <zyrinviacheslav@gmail.com>
2023-02-09 17:08:47 +0300

    authorizations script added

--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ToDoApp.views import ProjectModelViewSet
-from ToDoApp.views import UsersModelViewSet
-from ToDoApp.views import StatusModelViewSet
-from ToDoApp.views import ToDoModelViewSet
+from ToDo.ToDoApp.views import ProjectViewSet
+from ToDo.ToDoApp.views import UsersModelViewSet
+from ToDo.ToDoApp.views import StatusModelViewSet
+from ToDo.ToDoApp.views import ToDoModelViewSet
 
 router = DefaultRouter()
-router.register('Projest', ProjectModelViewSet)
+router.register('Projest', ProjectViewSet)
 router.register('User', UsersModelViewSet)
 router.register('Status', StatusModelViewSet)
 router.register('ToDo', ToDoModelViewSet)
@@ -16,4 +16,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
